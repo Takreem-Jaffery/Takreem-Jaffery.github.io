649722305102cdaa4f6288236f0ed0615f07cc7b
Takreem <takreem.jaffery@gmail.com>
2026-08-31 19:11:04 -0700

    Resolve Navigation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
-
+import { resolve } from 'path'
 // base: './' makes the build use relative asset paths, so it works whether
 // you deploy to a GitHub Pages *project* site (username.github.io/repo-name)
 // or a *user/org* site (username.github.io) without editing this file.
 export default defineConfig({
   base: '/',
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html')
+      }
+    }
   },
 });
